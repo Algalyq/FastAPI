12809bc90fb54ba2121ef91da275e24d67b40e38
Algalyq <algalyq@mail.ru>
2023-07-05 11:08:21 +0600

    upd: change url

--- app/config.py
+++ app/config.py
@@ -2,32 +2,36 @@ from typing import Any
 
 from pydantic import BaseSettings
 from pymongo import MongoClient
-
+from urllib.parse import quote_plus
 
 class Config(BaseSettings):
     CORS_ORIGINS: list[str] = ["*"]
     CORS_HEADERS: list[str] = ["*"]
     CORS_METHODS: list[str] = ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
 
-    MONGOHOST: str = "localhost"
-    MONGOPORT: str = "27017"
-    MONGOUSER: str = "root"
-    MONGOPASSWORD: str = "password"
-    MONGODATABASE: str = "fastapi"
-    MONGO_URL: str 
+    MONGOHOST: str
+    MONGOPORT: str
+    MONGOUSER: str
+    MONGOPASSWORD: str
+    MONGODATABASE: str
+    MONGO_URL: str
 
 # environmental variables
 env = Config()
-
-print(env.MONGO_URL)
-
+username = quote_plus(env.MONGOUSER)
+password = quote_plus(env.MONGOPASSWORD)
+mongo_url = "mongodb+srv://{username}:{password}@cluster0.zzr5jrn.mongodb.net/?retryWrites=true&w=majority"
+print(mongo_url)
 # FastAPI configurations
 fastapi_config: dict[str, Any] = {
     "title": "API test",
 }
 
+if env.MONGO_URL:
+    mongo_url = env.MONGO_URL
+    
 # MongoDB connection
-client = MongoClient(env.MONGO_URL)
+client = MongoClient(mongo_url)
 
 # MongoDB database
 database = client[env.MONGODATABASE]
