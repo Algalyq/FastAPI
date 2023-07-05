67cbd3595b5ac58dd8d482a6d15b8fdef554a9d5
Algalyq <algalyq@mail.ru>
2023-07-05 18:54:28 +0600

    upd: find bugs v3

--- app/api/adapters/openjourney_service.py
+++ app/api/adapters/openjourney_service.py
@@ -1,34 +1,28 @@
 import os
-from dotenv import dotenv_values
 import replicate
 import requests
 
-# env_vars = dotenv_values('.env')
-# replicate_api_token = env_vars.get('REPLICATE_API_TOKEN')
-# os.environ['REPLICATE_API_TOKEN'] = replicate_api_token
+
+replicate_api_token = os.environ.get('REPLICATE_API_TOKEN')
+
 class OpenJourney:
+
     def __init__(self):
-        pass
-
-    def create_images(self,image_url,n,prompt):
-        return ["asdd"]
-
-    # def __init__(self):
-    #     self.replicate = replicate.Client(replicate_api_token)
+        self.replicate = replicate.Client(replicate_api_token)
         
-    # def create_images(self, image_url,n,prompt):
-    #     image = image_url
+    def create_images(self, image_url,n,prompt):
+       
         
-    #     output = replicate.run(
-    #         "mbentley124/openjourney-img2img:c49a9422a0d4303e6b8a8d2cf35d4d1b1fd49d32b946f6d5c74b78886b7e5dc3",
-    #         input={
-    #             "image": image,
-    #             "prompt": prompt,
-    #             "strength": 0.5,
-    #             "num_images_per_prompt": 1
-    #         }
-    #     )
+        output = replicate.run(
+            "mbentley124/openjourney-img2img:c49a9422a0d4303e6b8a8d2cf35d4d1b1fd49d32b946f6d5c74b78886b7e5dc3",
+            input={
+                "image": image_url,
+                "prompt": prompt,
+                "strength": 0.5,
+                "num_images_per_prompt": 1
+            }
+        )
             
-    #     return output
+        return output
 
 
