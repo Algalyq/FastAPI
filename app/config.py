f6b85f47f60b5477d817e68a8ef68fc0bc6a2544
Algalyq <algalyq@mail.ru>
2023-07-05 11:20:00 +0600

    upd: change url2

--- app/config.py
+++ app/config.py
@@ -20,15 +20,13 @@ class Config(BaseSettings):
 env = Config()
 username = quote_plus(env.MONGOUSER)
 password = quote_plus(env.MONGOPASSWORD)
-mongo_url = "mongodb+srv://{username}:{password}@cluster0.zzr5jrn.mongodb.net/?retryWrites=true&w=majority"
+mongo_url = f"mongodb+srv://{username}:{password}@cluster0.zzr5jrn.mongodb.net/?retryWrites=true&w=majority"
 print(mongo_url)
 # FastAPI configurations
 fastapi_config: dict[str, Any] = {
     "title": "API test",
 }
 
-if env.MONGO_URL:
-    mongo_url = env.MONGO_URL
     
 # MongoDB connection
 client = MongoClient(mongo_url)
