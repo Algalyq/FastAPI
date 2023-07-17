8fcf301aba086a831d720d4fa6c618c5ae3df6b2
Algalyq <algalyq@mail.ru>
2023-07-17 15:50:39 +0600

    upd: llm gpt3.5 turbo

--- app/api/adapters/lang_service.py
+++ app/api/adapters/lang_service.py
@@ -15,28 +15,27 @@ class LangService:
 
     def test(self,query: str):
 
-        return query
-        # llm = OpenAI(api_key=openai_api_key, model='gpt-3.5-turbo',temperature=0)
+        llm = OpenAI(api_key=openai_api_key, model='gpt-3.5-turbo',temperature=0)
 
-        # tools = load_tools(["serpapi","llm-math","wolfram-alpha"],llm=llm,serpapi_api_key=serpapi,
-        #                                                                wolfram_alpha_appid=wolf)
+        tools = load_tools(["serpapi","llm-math","wolfram-alpha"],llm=llm,serpapi_api_key=serpapi,
+                                                                       wolfram_alpha_appid=wolf)
 
-        # agent = initialize_agent(tools,llm, agent="zero-shot-react-description",verbose=True)
+        agent = initialize_agent(tools,llm, agent="zero-shot-react-description",verbose=True)
 
-        # self.conversation.append({"role": "user", "content": query})
+        self.conversation.append({"role": "user", "content": query})
 
-        # # Append additional content to the conversation history
-        # self.conversation.append({"role": "assistant", "content": "This is an additional message from the assistant."})
+        # Append additional content to the conversation history
+        self.conversation.append({"role": "assistant", "content": "This is an additional message from the assistant."})
 
-        # # Append the conversation history to the messages for the agent
-        # messages = self.conversation
+        # Append the conversation history to the messages for the agent
+        messages = self.conversation
 
-        # # Generate a response from the agent
-        # response = agent.run(messages)
+        # Generate a response from the agent
+        response = agent.run(messages)
 
-        # # Extract and return the agent's reply
-        # reply = response['replies'][-1]
-        # return reply
+        # Extract and return the agent's reply
+        reply = response['replies'][-1]
+        return reply
 
 
-        # return agent.run(query)
+        return agent.run(query)
